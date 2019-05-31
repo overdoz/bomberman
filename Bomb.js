c68efa3c16fe568f6b7c3755dbcd6b75fe25b338
Angelos Kafounis <A.Kafounis@campus.lmu.de>
2019-05-31 20:11:04 +0200

    Implemented Class:  - Player  - Element  - Bomb - Constructor  - Grid - Constructor  - Game  - Wall - Constructor
     Added some photos of:
     - the Bomberman player
     - The bomb
     - The bomb after the explosion
     - another Bomberman player image
     - a Wall image
     - 2 photos of how the Grid could look like
    
     Created: Grid.html file that is going to represent the
     grid that the players are going to play. PS. I tried to make it work with AssetLoader but I have no Idea with the **** graphics
    
     Created a Folder for images + 2 Images are currently out
     of the folder so I can test them with the HTML Grid File.

--- Bomb.js
+++ Bomb.js
@@ -1,3 +1,12 @@
-class Item extends Element {
+const Element = require('./Element.js');
+
+class Bomb extends Element {
+
+    constructor(width, height, timeToExplode) {
+        super();
+        this.width = width;
+        this.height = height;
+    }
 
 }
+module.exports = Bomb;
--- Player.js
+++ Player.js
@@ -1,34 +1,88 @@
+const Element = require('./Element.js');
+
 class Player extends Element {
-    constructor(position, health, countBombs, countWalls) {
-        super(position);
-        this.health = health;
-        this.countBombs = countBombs;
-        this.countWalls = countWalls
+
+
+
+    constructor(position, health, numberOfBombs, numberOfWalls) {
+        super(position[0], position[1], null);
+        this.lifeLeft = health; // double
+        this.numberOfBombs = numberOfBombs; // 14 at the beginning
+        this.numberOfWalls = numberOfWalls; //  7 walls at the beginning
+        this.maximumNumberOfBombs = this.numberOfBombs*2;
+        this.powerUps = null;
+        this.timeLeftToBuildWall = 15;
+        this.dead = false;
+        this.direction = 'EAST';
     }
 
-    setBomb(position) {
-      //  test
-        this.setWall()
-
+    getGridPositionX() {
+        return super.getX();
     }
 
-    setWall(position) {
-
+    getGridPositionY() {
+        return super.getY();
     }
 
-    die() {
-
+    // Reduces the Life Left of the current Player
+    getDamage(damage) {
+        // damage must always be a negative double
+        if (damage >= 0) {
+            throw new Error("Damage must be a negative double number!");
+        }
+        this.lifeLeft += damage;
+        // Player is dead
+        if (this.lifeLeft <= 0) {
+            this.dead = true;
+        }
     }
 
-    dropLoot() {
-
+    throwBomb() {
+        // throws a bomb at the current position
+        if (this.numberOfBombs > 0) {
+            this.numberOfBombs--;
+            return new Bomb(this.getGridPositionX(), this.getGridPositionY());
+        }
+        return null;
     }
 
-    update() {
-
+    getNumberOfBombs() {
+        return this.numberOfBombs;
     }
 
-    draw() {
-
+    getLifeLeft() {
+        return this.lifeLeft;
     }
+
+    move(direction) {
+        // TODO: OUT OF CANVAS
+        // TODO: A WALL IN FRONT OF THE PLAYER
+        switch (direction) {
+            case "NORTH":
+                super.setY(super.getY() + 1);
+                return;
+            case "SOUTH":
+                super.setY(super.getY() - 1);
+                return;
+            case "EAST":
+                super.setX(super.getX() + 1);
+                return;
+            case "WEST":
+                super.setX(super.getX() - 1);
+                return;
+        }
+        throw new Error("Invalid Direction !!!");
+    }
+
+    buildWall() {
+        if (this.timeLeftToBuildWall == 0) {
+            this.timeLeftToBuildWall = 15;
+            return new Wall()
+        } else {
+            return null;
+        }
+    }
+
 }
+
+module.exports = Player;
